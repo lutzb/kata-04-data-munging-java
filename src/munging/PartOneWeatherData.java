422c203a037dd6501c3dd06f89093418b43cc83f
Lutz, Bryan <LUTZB1@nationwide.com>
2018-02-14 15:52:31 -0500

    Add implementation of PartOneWeatherData; Make test green;

--- src/munging/PartOneWeatherData.java
+++ src/munging/PartOneWeatherData.java
@@ -1,21 +1,56 @@
 package munging;
 
-import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class PartOneWeatherData {
-
-	public static void main(String[] args) throws FileNotFoundException {
-		int day = getSmallestTempSpreadDay();
-		System.out.println(day);
-	}
 	
 	public static int getSmallestTempSpreadDay() {
-		// TODO Output the day number (column 1) with the smallest temperature spread.
-		// The maximum temperature is the second column, the minimum the third column
-
+		InputStream inputStream = PartOneWeatherData.class.getResourceAsStream("weather.dat");
+		Scanner scanner = new Scanner(inputStream);
 		
-		return 0;
+		// Discard header and empty line (lines 1 and 2)
+		String headers = scanner.nextLine();
+		String emptyLine = scanner.nextLine();
+		
+		int day = 0;
+		int largestSpread = 0;
+		while(scanner.hasNext()) {
+			String line1 = scanner.nextLine();
+			
+			String normalizedLine = line1.replaceAll("\\s+", " ");
+			List<String> lineArray = Arrays.asList(normalizedLine.split(" "));
+			
+			String currentDay = lineArray.get(1);
+			
+			if (!currentDay.equals("mo")) {				
+				int currentMax = sanitizeInput(lineArray.get(2));
+				int currentMin = sanitizeInput(lineArray.get(3));
+				
+				System.out.println("Current Line Data: " + currentDay + " " + currentMax + " " +  currentMin);
+				int currentSpread = currentMax - currentMin;
+				
+				if (currentSpread > largestSpread) {
+					largestSpread = currentSpread;
+					day = Integer.parseInt(currentDay);
+				}
+			}
+		}
+		
+		scanner.close();
+		
+		return day;
 	}
 
+	private static int sanitizeInput(String input) {
+		String result = "";
+		for (char c : input.toCharArray()) {
+			if (Character.isDigit(c)) {
+				result += c;
+			}
+		}
+		return Integer.parseInt(result);
+	}
 }
